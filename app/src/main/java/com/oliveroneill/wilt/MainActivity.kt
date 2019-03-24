bc0bac3fe8c1caa134bf369bb8a854694577281f
Oliver <oliveroneill04@gmail.com>
2019-03-25 08:06:20 +1100

    Fix results not being passed through to fragment

--- app/src/main/java/com/oliveroneill/wilt/MainActivity.kt
+++ app/src/main/java/com/oliveroneill/wilt/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.oliveroneill.wilt
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // Send result to relevant fragments in navigation
+        (supportFragmentManager.fragments.first() as? NavHostFragment)?.let {
+            it.childFragmentManager.fragments.forEach {
+                it.onActivityResult(requestCode, resultCode, data)
+            }
+        }
     }
 }
