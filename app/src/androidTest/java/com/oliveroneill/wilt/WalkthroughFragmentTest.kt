cc846273ef5beef62aabedf3d955b7d0518820fc
Oliver <oliveroneill04@gmail.com>
2019-04-10 21:59:45 +1000

    Cleanup

--- app/src/androidTest/java/com/oliveroneill/wilt/WalkthroughFragmentTest.kt
+++ app/src/androidTest/java/com/oliveroneill/wilt/WalkthroughFragmentTest.kt
@@ -3,12 +3,14 @@ package com.oliveroneill.wilt
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
+import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
@@ -26,21 +28,37 @@ import org.mockito.Mockito.*
 @RunWith(AndroidJUnit4::class)
 class WalkthroughFragmentTest {
     // Create fake view model for sending events to the UI
-    val model = mock(WalkthroughFragmentViewModel::class.java)
+    val viewModel = mock(WalkthroughFragmentViewModel::class.java)
     val stateData = MutableLiveData<Event<WalkthroughFragmentState>>()
     // Create factory that returns the fake view model
     val factory = mock(ViewModelProvider.AndroidViewModelFactory::class.java)
     val navController = mock(NavController::class.java)
+    lateinit var scenario: FragmentScenario<WalkthroughFragment>
 
     @Before
     fun setup() {
-        `when`(factory.create(WalkthroughFragmentViewModel::class.java)).thenReturn(model)
-        `when`(model.state).thenReturn(stateData)
+        `when`(factory.create(WalkthroughFragmentViewModel::class.java)).thenReturn(viewModel)
+        `when`(viewModel.state).thenReturn(stateData)
+        // Specify the fragment factory in order to set the view model factory
+        scenario = launchFragmentInContainer<WalkthroughFragment>(
+            null,
+            R.style.AppTheme,
+            object : FragmentFactory() {
+                @Suppress("UNCHECKED_CAST")
+                override fun instantiate(classLoader: ClassLoader, className: String, args: Bundle?): Fragment {
+                    return (super.instantiate(classLoader, className, args) as WalkthroughFragment).also {
+                        it.viewModelFactory = factory
+                    }
+                }
+            }
+        )
+        scenario.onFragment {
+            Navigation.setViewNavController(it.requireView(), navController)
+        }
     }
 
     @Test
     fun shouldDisplayWalkthrough() {
-        launchFragmentInContainer<WalkthroughFragment>()
         onView(
             allOf(
                 withId(R.id.textView),
@@ -50,23 +68,10 @@ class WalkthroughFragmentTest {
     }
 
     @Test
-    fun shouldOpenSpotifyLoginOnClick() {
+    fun shouldOpenSpotifyLoginEvent() {
         // Set login state
         val request = SpotifyAuthenticationRequest("", "x", emptyArray())
         stateData.postValue(Event(WalkthroughFragmentState.LoggingIn(request)))
-        // Specify the fragment factory in order to set the view model factory
-        launchFragmentInContainer<WalkthroughFragment>(
-            null,
-            R.style.AppTheme,
-            object : FragmentFactory() {
-                @Suppress("UNCHECKED_CAST")
-                override fun instantiate(classLoader: ClassLoader, className: String, args: Bundle?): Fragment {
-                    return (super.instantiate(classLoader, className, args) as WalkthroughFragment).also {
-                        it.viewModelFactory = factory
-                    }
-                }
-            }
-        )
         // Ensure that the walkthrough screen is hidden since the Spotify login screen should open
         onView(withId(R.id.textView)).check(doesNotExist())
         // Not sure whether there's something else to assert here since it's not my view that's opening...
@@ -74,24 +79,14 @@ class WalkthroughFragmentTest {
 
     @Test
     fun shouldShowLoginError() {
-        // Specify the fragment factory in order to set the view model factory
-        val scenario = launchFragmentInContainer<WalkthroughFragment>(
-            null,
-            R.style.AppTheme,
-            object : FragmentFactory() {
-                @Suppress("UNCHECKED_CAST")
-                override fun instantiate(classLoader: ClassLoader, className: String, args: Bundle?): Fragment {
-                    return (super.instantiate(classLoader, className, args) as WalkthroughFragment).also {
-                        it.viewModelFactory = factory
-                    }
-                }
-            }
-        )
-        scenario.onFragment { fragment ->
-            Navigation.setViewNavController(fragment.requireView(), navController)
-        }
         // Set error state
         stateData.postValue(Event(WalkthroughFragmentState.LoginError("Something bad happened")))
         verify(navController).navigate(eq(R.id.action_login_failure))
+    }
+
+    @Test
+    fun shouldOpenSpotifyLoginOnClick() {
+        onView(withId(R.id.signInButton)).perform(click())
+        verify(viewModel).spotifySignup()
     }
 }
